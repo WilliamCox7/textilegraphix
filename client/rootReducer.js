b9d7cabaa3b27f6dfa21bfc97f7ce74246a5d8af
William Cox <willubyu7@gmail.com>
2017-09-21 21:00:35 -0600

    starting to ad product reducer

--- client/rootReducer.js
+++ client/rootReducer.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux';
 import shop from './reducers/shop';
 import cart from './reducers/cart';
 import nav from './reducers/nav';
+import product from './reducers/product';
 
 export default combineReducers({
-  shop, cart, nav
+  shop, cart, nav, product
 });
