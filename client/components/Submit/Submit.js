4ae96a5bf915887993113171b2c99944927f49bd
William Cox <willubyu7@gmail.com>
2017-10-09 19:33:58 -0600

    mapped inputs to state

--- client/components/Submit/Submit.js
+++ client/components/Submit/Submit.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import emptyBox from '../../src/empty-box.svg';
+import radioEmpty from '../../src/radio-empty.svg';
+import radioFilled from '../../src/radio-filled.svg';
 import Mockup from '../Mockup/Mockup';
 import Summary from '../Summary/Summary';
 import './Submit.scss';
 
 class Submit extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      projectName: '',
+      first: '',
+      last: '',
+      company: '',
+      phone: '',
+      email: '',
+      confirm: '',
+      companyName: '',
+      attn: '',
+      address1: '',
+      address2: '',
+      city: '',
+      state: '',
+      zip: '',
+      pickup: false,
+      notes: ''
+    }
+    this.togglePickup = this.togglePickup.bind(this);
+    this.update = this.update.bind(this);
+  }
+
+  togglePickup() {
+    this.setState({pickup: !this.state.pickup});
+  }
+
+  update(e) {
+    var newState = Object.assign({}, this.state);
+    newState[e.target.name] = e.target.value;
+    this.setState(newState);
+  }
+
   render() {
 
     var files = this.props.modal.images.map((file, i) => {
@@ -15,12 +51,13 @@ class Submit extends Component {
     var quotes = this.props.cart.products.map((product, i) => {
       return <Summary summary={product} key={i} />;
     });
-    
+
     return (
       <div className="Submit">
         <div className="left-side">
           <h1>Quote Submission Form</h1>
-          <input className="project-name" type="text" placeholder="Project Name" />
+          <input className="project-name" type="text" placeholder="Project Name"
+            value={this.state.projectName} onChange={this.update} name="projectName" />
           <h4>Uploaded Files</h4>
           <h5>We recommend AI or PSD files, sized to print or 300DPI</h5>
           <div className="files">
@@ -29,30 +66,45 @@ class Submit extends Component {
           <button>+ Upload</button>
           <h4>Contact Information</h4>
           <div className="input-section">
-            <input type="text" placeholder="*First" />
-            <input type="text" placeholder="*Last" />
-            <input type="text" placeholder="Company" />
-            <input type="text" placeholder="*Phone" />
-            <input type="text" placeholder="*Email" />
-            <input type="text" placeholder="*Confirm Email" />
+            <input type="text" placeholder="*First"
+              value={this.state.first} onChange={this.update} name="first" />
+            <input type="text" placeholder="*Last"
+              value={this.state.last} onChange={this.update} name="last" />
+            <input type="text" placeholder="Company"
+              value={this.state.company} onChange={this.update} name="company" />
+            <input type="text" placeholder="*Phone"
+              value={this.state.phone} onChange={this.update} name="phone" />
+            <input type="text" placeholder="*Email"
+              value={this.state.email} onChange={this.update} name="email" />
+            <input type="text" placeholder="*Confirm Email"
+              value={this.state.confirm} onChange={this.update} name="confirm" />
           </div>
           <h4>Shipping Information</h4>
           <div className="input-section">
-            <input type="text" placeholder="*Company / Name" />
-            <input type="text" placeholder="attn:" />
-            <input type="text" placeholder="*Address 1" />
-            <input type="text" placeholder="Address 2" />
-            <input type="text" placeholder="*City" />
+            <input type="text" placeholder="*Company / Name"
+              value={this.state.companyName} onChange={this.update} name="companyName" />
+            <input type="text" placeholder="attn:"
+              value={this.state.attn} onChange={this.update} name="attn" />
+            <input type="text" placeholder="*Address 1"
+              value={this.state.address1} onChange={this.update} name="address1" />
+            <input type="text" placeholder="Address 2"
+              value={this.state.address2} onChange={this.update} name="address2" />
+            <input type="text" placeholder="*City"
+              value={this.state.city} onChange={this.update} name="city" />
             <div className="shared-inputs">
-              <input type="text" placeholder="*State" />
-              <input type="text" placeholder="*Zip" />
+              <input type="text" placeholder="*State"
+                value={this.state.state} onChange={this.update} name="state" />
+              <input type="text" placeholder="*Zip"
+                value={this.state.zip} onChange={this.update} name="zip" />
             </div>
           </div>
           <div className="checkbox-section">
-            <img src={emptyBox} />
+            <img src={this.state.pickup ? radioFilled : radioEmpty}
+              onClick={this.togglePickup} />
             Pick Up in Rexburg, Idaho
           </div>
-          <textarea placeholder="Notes..."></textarea>
+          <textarea placeholder="Notes..." value={this.state.notes}
+            onChange={this.update} name="notes"></textarea>
           <h4>Total + Estimated Delivery</h4>
           <div className="details">
             <h5>Est. Total with Taxes + Shipping</h5>
