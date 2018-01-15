e5671aa452eed45996aa4840cf6e3f283972ed8d
William Cox <williamcox@Williams-MBP.lan>
2018-01-15 00:31:28 -0700

    refactored home component

---
+++ client/components/Home/modules/construct.js
@@ -0,0 +1,5 @@
+export function construct(Home) {
+  Home.state = {
+    width: document.body.clientWidth
+  }
+}
--- client/packages.js
+++ client/packages.js
@@ -5,8 +5,9 @@ import ReactDOM from "react-dom";
 import { Router, Route, hashHistory } from "react-router";
 import thunk from 'redux-thunk';
 import axios from 'axios';
+import { Parallax, Background } from 'react-parallax';
 
 export {
   React, Component, connect, Provider, combineReducers, createStore, applyMiddleware,
-  compose, ReactDOM, Router, Route, hashHistory, thunk, axios
+  compose, ReactDOM, Router, Route, hashHistory, thunk, axios, Parallax, Background
 }
