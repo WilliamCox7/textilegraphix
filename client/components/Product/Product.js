f2d70f120986c0a6ccce11c054c77a410cbb8985
William Cox <williamcox@Williams-MBP.lan>
2018-01-15 23:10:25 -0700

    refactored Product component

--- client/components/Product/Product.js
+++ client/components/Product/Product.js
@@ -1,40 +1,18 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { setModal } from '../../reducers/modal';
-import { setProduct } from '../../reducers/product';
+import { Component, connect } from '../../packages';
+import { template, construct, mapDispatchToProps } from './modules';
 import './Product.scss';
 
 class Product extends Component {
 
   constructor() {
     super();
-    this.openModal = this.openModal.bind(this);
-  }
-
-  openModal(product) {
-    this.props.setModal(true);
-    this.props.setProduct(product);
+    construct(this);
   }
 
   render() {
-    return (
-      <div className="Product">
-        <div className="image-container">
-          <img src={this.props.product.image} />
-          <button onClick={() => this.openModal(this.props.product)}>
-            Build Product
-          </button>
-        </div>
-        <h1>{this.props.product.brand.toUpperCase()}</h1>
-        <h1>{this.props.product.number}</h1>
-      </div>
-    );
+    return template(this);
   }
-}
 
-const mapDispatchToProps = {
-  setModal: setModal,
-  setProduct: setProduct
 }
 
 export default connect(null, mapDispatchToProps)(Product);
---
+++ client/components/Product/modules/map-dispatch-to-props.js
@@ -0,0 +1,7 @@
+import { setModal } from '../../../reducers/modal';
+import { setProduct } from '../../../reducers/product';
+
+export const mapDispatchToProps = {
+  setModal: setModal,
+  setProduct: setProduct
+}
