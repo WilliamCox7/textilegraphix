b3943a0ee67a77abd7e70f130a61dac6f14f8913
William Cox <williamcox@Williams-MBP.lan>
2018-01-22 19:33:26 -0700

    conflicts

--- client/components/Nav/Nav.js
+++ client/components/Nav/Nav.js
@@ -10,11 +10,94 @@ class Nav extends Component {
   }
 
   componentDidMount() {
+<<<<<<< HEAD
     componentDidMount(this);
   }
 
   render() {
     return template(this);
+=======
+    var location = hashHistory.getCurrentLocation().pathname;
+    location = location.split("/")[1];
+    this.props.setLocation(location);
+    window.addEventListener("click", (e) => {
+      if (e.target.className !== 'search-bar' && e.target.className !== 'search-button') {
+        this.setState({searchActive: false});
+      } 
+      if (e.target.className === 'submit') {
+        this.closeQuoteBox();
+      }
+    });
+  }
+
+  closeQuoteBox() {
+    this.setState({showQuoteBox: false});
+  }
+
+  showQuoteBox() {
+    this.setState({showQuoteBox: !this.state.showQuoteBox});
+  }
+
+  setLocation(e) {
+    var location = e.target.innerText.toLowerCase();
+    this.props.setLocation(location);
+  }
+
+  setSearch(e) {
+    this.setState({searchActive: !this.state.searchActive});
+    e.currentTarget.parentElement.children[1].children[0].focus();
+    this.closeQuoteBox();
+  }
+
+  updateSrch(e) {
+    this.setState({searchTxt: e.target.value});
+    this.closeQuoteBox();
+  }
+
+  resetSearchText() {
+    this.setState({searchTxt: ''});
+  }
+
+  render() {
+    return (
+      <div className="Nav">
+        {this.state.width > 600 ? (
+          <div className="nav-container-desktop">
+            <a href="/#/" onClick={this.setLocation}>
+              <img className="logo" src={logo} />
+            </a>
+            <div className="links">
+              <a style={this.props.nav.location === "contact" ? {color: '#44B1DE'} : null} 
+                href="/#/contact" onClick={this.setLocation}>CONTACT</a>
+              <a style={this.props.nav.location === "shop" ? {color: '#44B1DE'} : null} 
+                 href="/#/shop" onClick={this.setLocation}>SHOP</a>
+              <div className="search">
+                <img onClick={this.setSearch} className="search-button" src={search} />
+                <form>
+                  <input className="search-bar" type="text" value={this.state.searchTxt} 
+                    style={this.state.searchActive ? {
+                      width: '150px'
+                  } : null} onChange={this.updateSrch} />
+                </form>
+                {this.state.searchTxt && this.state.searchActive ? (
+                  <SearchResults searchTxt={this.state.searchTxt}
+                    resetSearchText={this.resetSearchText} />
+                ) : null}
+              </div>
+              {this.props.cart.products.length > 0 ? (
+                <img onClick={this.showQuoteBox} className="quote-icon" src={quoteBlue} />
+              ) : (
+                <img onClick={this.showQuoteBox} className="quote-icon" src={quoteGray} />
+              )}
+              {this.state.showQuoteBox ? (<QuoteBox />) : null}
+            </div>
+          </div>
+        ) : (
+          "Mobile"
+        )}
+      </div>
+    );
+>>>>>>> parent of aef9be6... fixed search links
   }
 
 }
--- client/components/SearchResults/SearchResults.js
+++ client/components/SearchResults/SearchResults.js
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 import { Component, connect } from '../../packages';
 import { template, mapStateToProps, mapDispatchToProps, construct } from './modules';
+=======
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setModal } from '../../reducers/modal';
+import { setProduct } from '../../reducers/product';
+>>>>>>> parent of aef9be6... fixed search links
 import './SearchResults.scss';
 
 class SearchResults extends Component {
@@ -10,9 +17,43 @@ class SearchResults extends Component {
   }
 
   render() {
+<<<<<<< HEAD
     return template(this);
+=======
+
+    var count = 0, results = [];
+    this.props.shop.products.forEach((product, i) => {
+      if (JSON.stringify(product).indexOf(this.props.searchTxt) > -1 && count < 7) {
+        count++;
+        results.push(
+          <div className="result" key={i} onClick={() => this.selectProduct(product)}>
+            <img src={product.image} />
+            <span>
+              {product.brand}
+            </span>
+            <span>
+              {product.number}
+            </span>
+          </div>
+        );
+      }
+    });
+
+    return (
+      <div className="SearchResults">
+        {results.length > 0 ? results : (
+          <div className="error">no results for "{this.props.searchTxt}"</div>)}
+      </div>
+    );
+>>>>>>> parent of aef9be6... fixed search links
   }
 
+<<<<<<< HEAD
+=======
+const mapDispatchToProps = {
+  setModal: setModal,
+  setProduct: setProduct
+>>>>>>> parent of aef9be6... fixed search links
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchResults);
