f211440e971f612bb8134f323c0e5a2c1a91346a
William Cox <williamcox@Williams-MBP.lan>
2018-03-27 00:10:56 -0600

    nav barebones

---
+++ client/assets/index.js
@@ -0,0 +1,7 @@
+import logoTextBlack from './logo-text-black.svg';
+import searchIcon from './search-icon.svg';
+import shoppingCart from './shopping-cart.svg';
+
+export {
+  logoTextBlack, searchIcon, shoppingCart
+}
--- client/components/Nav/index.js
+++ client/components/Nav/index.js
@@ -1,11 +1,31 @@
-import { React, Component } from '../../packages';
+import { React, Component, Link } from '../../packages';
+import { logoTextBlack, searchIcon, shoppingCart } from '../../assets';
 import './style.scss';
 
 class Nav extends Component {
   render() {
     return (
-      <div className="Nav">
-        Nav
+      <div className="Nav flex jc-sb ai-c">
+        <Link to="/">
+          <img className="logo" src={logoTextBlack} />
+        </Link>
+        <div className="flex ai-c">
+          <div className="routes">
+            <Link to="products">
+              Products
+            </Link>
+            <Link to="support">
+              Support
+            </Link>
+          </div>
+          <div className="search">
+            <img src={searchIcon} />
+          </div>
+          <Link to="order" className="cart">
+            <img src={shoppingCart} />
+            <span>0</span>
+          </Link>
+        </div>
       </div>
     );
   }
