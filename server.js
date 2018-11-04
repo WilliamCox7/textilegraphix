b17a493335e14ca302fd0bd99083a073001f68ef
William Cox <willubyu7@gmail.com>
2018-11-03 19:26:50 -0600

    added ssl

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const https = require('https');
+const fs = require('fs');
+const config = require('./config');
 const app = module.exports = express();
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 3001));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(express.static(__dirname + '/build'));
 
@@ -17,6 +20,16 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './build/index.html'));
 });
 
-app.listen(app.get('port'), () => {
+const options = {
+  key: fs.readFileSync(config.ssl.key),
+  cert: fs.readFileSync(config.ssl.crt)
+};
+
+if (process.env.NODE_ENV === 'production') {
+  options.ca = fs.readFileSync(config.ssl.ca);
+}
+
+https.createServer(options, app)
+.listen(app.get('port'), () => {
   console.log('localhost:' + app.get('port'));
 });
