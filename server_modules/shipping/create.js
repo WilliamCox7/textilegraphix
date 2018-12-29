9c736c17400b88979c31f4dc033b7c1dccd3ffbe
William Cox <willubyu7@gmail.com>
2018-12-28 20:45:22 -0700

    lbs to oz

--- server_modules/shipping/create.js
+++ server_modules/shipping/create.js
@@ -5,19 +5,19 @@ const ErrorModule = require('../error');
 
 module.exports = function create(postBody) {
   let toAddress = new api.Address({ zip: postBody.zip });
-    let fromAddress = new api.Address({
-      street1: "1154 Stocks Ave",
-      city: "Rexburg",
-      state: "ID",
-      zip: "83440",
-      country: "US"
-    });
-    let parcel = new api.Parcel({ weight: postBody.weight });
-    let shipment = new api.Shipment({
-      to_address: toAddress,
-      from_address: fromAddress,
-      parcel: parcel
-    });
-    return shipment.save()
-    .catch((err) => ErrorModule.handle(err, 'B-002'));
+  let fromAddress = new api.Address({
+    street1: "1154 Stocks Ave",
+    city: "Rexburg",
+    state: "ID",
+    zip: "83440",
+    country: "US"
+  });
+  let parcel = new api.Parcel({ weight: postBody.weight * 16 });
+  let shipment = new api.Shipment({
+    to_address: toAddress,
+    from_address: fromAddress,
+    parcel: parcel
+  });
+  return shipment.save()
+  .catch((err) => ErrorModule.handle(err, 'B-002'));
 }
